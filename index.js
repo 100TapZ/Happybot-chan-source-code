1771dfeecf48bb1b1ac1a4f1623a98858311cae0
Sadboiako420 <89244986+Sadboiako420@users.noreply.github.com>
2021-10-05 21:27:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 
 
 setInterval(async () => {
-  await fetch('https://Happybot-chan.remoel-lowielow.repl.co').then(console.log('Pinged!'))
+  await fetch('replit live link here').then(console.log('Pinged!'))
 }, 200000);
 
 client.build();
